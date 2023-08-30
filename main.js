58b25bbe2f7da82c96eb5a1687bfba26dee1c522
Natalia Pavlyk <117844787+tasha201@users.noreply.github.com>
2023-08-30 21:14:43 +0300

    Add files via upload
    GoIT html-css-bootcamp

---
+++ main.js
@@ -0,0 +1,69 @@
+const title = document.querySelector(".cats-title");
+const logotype = document.querySelector(".site-logo img");
+const body = document.querySelector("body");
+
+function onLogoClick() {
+  logotype.classList.toggle("is-hidden");
+}
+title.addEventListener("click", onLogoClick);
+
+const catPhoto = document.querySelector(".cat-photo");
+
+function onPhotoClick() {
+  fetch("https://loremflickr.com/320/240").then((response) => {
+    const imgUrl = response.url;
+    catPhoto.src = imgUrl;
+    catPhoto.width = "296";
+    catPhoto.height = "370";
+  })
+    // catPhoto.src = "https://picsum.photos/200/300";
+    .catch((error) => {
+      console.error(error);
+    });
+}
+catPhoto.addEventListener("click", onPhotoClick);
+
+// title.classList.remove("cats-title");
+
+// console.log(title);
+
+// title.style.cssText = "color: tomato; font-size: 50px";
+// title.style.fontSize = "50px";
+
+// String
+// Number
+// Boolean
+// Null
+// Undefined
+// Symbol
+// bigInt
+// Object
+
+const slider = document.querySelector(".hero-list");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+const mySiema = new Siema({
+  selector: slider,
+  loop: true,
+  duration: 1000,
+  easing: "cubic-bezier(.17,.67,.32,1.34)"
+});
+
+function onNextClick() {
+  mySiema.next();
+}
+
+function onPrevClick() {
+  mySiema.prev();
+}
+
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  closeText: "&#128514;",
+  fadeSpeed: 1000
+});
+
+AOS.init();
